7205905604b44169caa2bedf4c0041df776e0b2e
Nathan Vinicius <limavinicius195@gmail.com>
2025-06-17 19:22:52 -0300

    novo commit

---
+++ Arquivos.py
@@ -0,0 +1,14 @@
+import csv
+
+
+caminho_do_arquivo: str = "exemplo.csv"
+
+arquivo_csv: list = []
+
+
+with open(file=caminho_do_arquivo,mode="r", encoding='utf-8') as arquivos:
+  leitor_csv = csv.DictReader(arquivos)
+  
+  for linha in leitor_csv:
+      arquivo_csv.append(linha)
+      print(arquivo_csv)
